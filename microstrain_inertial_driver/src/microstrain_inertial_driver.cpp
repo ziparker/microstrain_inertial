5759792752d571dd71bc08174ccd77c0b04b6071
robbiefish <robbie.fish76@gmail.com>
2021-10-20 20:17:12 +0000

    Updates submodules and activates new publishers

--- microstrain_inertial_driver/src/microstrain_inertial_driver.cpp
+++ microstrain_inertial_driver/src/microstrain_inertial_driver.cpp
@@ -205,6 +205,9 @@ bool Microstrain::activate_node()
 
     if(publishers_.gnss_aiding_status_pub_[i])
       publishers_.gnss_aiding_status_pub_[i]->on_activate();
+
+    if(publishers_.gnss_fix_info_pub_[i])
+      publishers_.gnss_fix_info_pub_[i]->on_activate();
   }
 
   //RTK Data publisher
@@ -230,6 +233,9 @@ bool Microstrain::activate_node()
   if(publishers_.filter_relative_pos_pub_)
     publishers_.filter_relative_pos_pub_->on_activate();
   
+  if(publishers_.filter_aiding_measurement_summary_pub_)
+    publishers_.filter_aiding_measurement_summary_pub_->on_activate();
+
   if(publishers_.gnss_dual_antenna_status_pub_)
     publishers_.gnss_dual_antenna_status_pub_->on_activate();
   
@@ -277,6 +283,9 @@ bool Microstrain::deactivate_node()
 
     if(publishers_.gnss_aiding_status_pub_[i])
       publishers_.gnss_aiding_status_pub_[i]->on_deactivate();
+
+    if(publishers_.gnss_fix_info_pub_[i])
+      publishers_.gnss_fix_info_pub_[i]->on_deactivate();
   }
 
   //RTK Data publisher
@@ -301,6 +310,9 @@ bool Microstrain::deactivate_node()
 
   if(publishers_.filter_relative_pos_pub_)
     publishers_.filter_relative_pos_pub_->on_deactivate();
+
+  if(publishers_.filter_aiding_measurement_summary_pub_)
+    publishers_.filter_aiding_measurement_summary_pub_->on_deactivate();
 
   if(publishers_.gnss_dual_antenna_status_pub_)
     publishers_.gnss_dual_antenna_status_pub_->on_deactivate();
@@ -351,6 +363,9 @@ bool Microstrain::shutdown_or_cleanup_node()
 
     if(publishers_.gnss_aiding_status_pub_[i])
       publishers_.gnss_aiding_status_pub_[i].reset();
+
+    if(publishers_.gnss_fix_info_pub_[i])
+      publishers_.gnss_fix_info_pub_[i].reset();
   }
 
   //RTK Data publisher
@@ -375,6 +390,9 @@ bool Microstrain::shutdown_or_cleanup_node()
 
   if(publishers_.filter_relative_pos_pub_)
     publishers_.filter_relative_pos_pub_.reset();
+
+  if(publishers_.filter_aiding_measurement_summary_pub_)
+    publishers_.filter_aiding_measurement_summary_pub_.reset();
 
   if(publishers_.gnss_dual_antenna_status_pub_)
     publishers_.gnss_dual_antenna_status_pub_.reset();
